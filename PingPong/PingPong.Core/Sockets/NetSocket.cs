d96f0735b36ae71daffaded31b6b59aa471b79b8
almog gueta <almog.gueta1741@gmail.com>
2022-01-19 16:46:58 +0200

    Added NetSocket implementation of SocketBase

---
+++ PingPong/PingPong.Core/Sockets/NetSocket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Core.Sockets
+{
+    public class NetSocket : SocketBase
+    {
+        private Socket _socket;
+
+        public NetSocket(SocketType socketType, ProtocolType protocolType, IPEndPoint iPEndPoint)
+        {
+            _socket = new Socket(socketType, protocolType);
+            _socket.Connect(iPEndPoint);
+        }
+
+        public override void Send(byte[] data)
+        {
+            _socket.Send(data);
+        }
+
+        public override byte[] Receive()
+        {
+            byte[] data = new byte[1024];
+            _socket.Receive(data);
+            return data;
+        }
+    }
+}
