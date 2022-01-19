560b71b47118a448d9c051443ffdd03f611a1d90
almog gueta <almog.gueta1741@gmail.com>
2022-01-19 17:45:05 +0200

    Added a server class

---
+++ PingPong/PingPong.Core/Server.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using Core.Sockets.Abstractions;
+
+namespace Core
+{
+    public class Server
+    {
+        public IPEndPoint EndPoint { get; }
+        public IListeningSocket ListeningSocket;
+
+        public Server(IListeningSocket listeningSocket, IPEndPoint iPEndPoint)
+        {
+            EndPoint = iPEndPoint;
+            ListeningSocket = listeningSocket;
+        }
+    }
+}
