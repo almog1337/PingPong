7d4ad5d3c5a198dbf92000e72a46a95d02d1250f
almog gueta <almog.gueta1741@gmail.com>
2022-01-19 17:21:58 +0200

    Added implementation of ListeningSocketBase

---
+++ PingPong/PingPong.Core/Sockets/NetListeningSocket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Sockets.Abstractions;
+
+namespace Core.Sockets
+{
+    public class NetListeningSocket : ListeningSocketBase
+    {
+        private Socket _socket;
+        private IPEndPoint _bindEndPoint;
+
+        public NetListeningSocket(SocketType socketType, ProtocolType protocolType, IPEndPoint iPEndPoint)
+        {
+            _socket = new Socket(socketType, protocolType);
+            
+        }
+
+        public void Bind()
+        {
+            _socket.Bind(_bindEndPoint);
+        }
+
+        public void Listen()
+        {
+            _socket.Listen();
+        }
+
+        public SocketBase Accept()
+        {
+            var sock = _socket.Accept();
+            return new NetSocket(sock);
+        }
+    }
+}
