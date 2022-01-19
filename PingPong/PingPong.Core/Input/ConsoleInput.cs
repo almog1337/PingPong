302126879b378861fa09f781f54a03262668758d
almog gueta <almog.gueta1741@gmail.com>
2022-01-19 16:43:27 +0200

    Added COnsoleInput implementation of IINput

---
+++ PingPong/PingPong.Core/Input/ConsoleInput.cs
@@ -0,0 +1,15 @@
+﻿using Core.Input.Abstractions;
+using System;
+using System.Text;
+
+namespace Core.Input
+{
+    public class ConsoleInput : IInput
+    {
+        public byte[] Receive()
+        {
+            string input = Console.ReadLine();
+            return Encoding.ASCII.GetBytes(input);
+        }
+    }
+}
