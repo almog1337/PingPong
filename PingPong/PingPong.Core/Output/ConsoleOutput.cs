1e6ca9905848cd3b23ea2e070b93c7561a44b65f
almog gueta <almog.gueta1741@gmail.com>
2022-01-19 16:44:59 +0200

    Added ConsoleOutput implementation of IOutput

---
+++ PingPong/PingPong.Core/Output/ConsoleOutput.cs
@@ -0,0 +1,15 @@
+﻿using Core.Output.Abstractions;
+using System;
+using System.Text;
+
+namespace Core.Output
+{
+    public class ConsoleOutput : IOutput
+    {
+        public void Send(byte[] data)
+        {
+            string dataString = Encoding.ASCII.GetString(data);
+            Console.WriteLine(dataString);
+        }
+    }
+}
