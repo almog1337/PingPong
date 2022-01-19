5bafb84e4168a6009529b8471c92387dbf51c1ef
almog gueta <almog.gueta1741@gmail.com>
2022-01-19 17:52:56 +0200

    Added Client project with Bootstraper class

---
+++ PingPong/Client/Bootstraper.cs
@@ -0,0 +1,20 @@
+﻿using Core;
+using Core.Sockets;
+using System.Net;
+using System.Net.Sockets;
+
+namespace PingPongClient
+{
+    public class Bootstraper
+    {
+        public Client CreateClient(string[] args)
+        {
+            IPEndPoint iPEndPoint;
+            IPEndPoint.TryParse(args[0], out iPEndPoint);
+
+            NetSocket netSocket = new NetSocket(SocketType.Stream, ProtocolType.Tcp, iPEndPoint);
+
+            return new Client(iPEndPoint, netSocket);
+        }
+    }
+}
