cc8b97c3661baeb8b6d12f7a508445c3db7de73e
almog gueta <almog.gueta1741@gmail.com>
2022-01-19 16:46:30 +0200

    Added socketBase abstract class

---
+++ PingPong/PingPong.Core/Sockets/Abstractions/SocketBase.cs
@@ -0,0 +1,13 @@
+﻿using Core.Input.Abstractions;
+using Core.Output.Abstractions;
+using System;
+using System.Threading.Tasks;
+
+namespace Core.Sockets
+{
+    public abstract class SocketBase : IInput, IOutput
+    {
+        public abstract void Send(byte[] data);
+        public abstract byte[] Receive();
+    }
+}
