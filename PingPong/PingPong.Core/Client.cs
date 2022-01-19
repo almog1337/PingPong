bd6e83a9b45285a2819342e8bac1a46e67a171b6
almog gueta <almog.gueta1741@gmail.com>
2022-01-19 16:49:04 +0200

    Added Client class

---
+++ PingPong/PingPong.Core/Client.cs
@@ -0,0 +1,17 @@
+﻿using Core.Sockets;
+using System.Net;
+
+namespace Core
+{
+    public class Client
+    {
+        public IPEndPoint _endPoint { get; }
+        public  SocketBase _socket;
+
+        public Client(IPEndPoint endPoint, SocketBase socket)
+        {
+            _endPoint = endPoint;
+            _socket = socket;
+        }
+    }
+}
