ba5761d62e7bb6aafd345eea3bc99c0a1c9470ce
almog gueta <almog.gueta1741@gmail.com>
2022-01-20 09:13:19 +0200

    Added connect and close functions to SocketBase

--- PingPong/PingPong.Core/Sockets/Abstractions/SocketBase.cs
+++ PingPong/PingPong.Core/Sockets/Abstractions/SocketBase.cs
@@ -1,13 +1,13 @@
 ﻿using Core.Input.Abstractions;
 using Core.Output.Abstractions;
-using System;
-using System.Threading.Tasks;
 
 namespace Core.Sockets
 {
     public abstract class SocketBase : IInput, IOutput
     {
+        public abstract void Connect();
         public abstract void Send(byte[] data);
         public abstract byte[] Receive();
+        public abstract void Close();
     }
 }
