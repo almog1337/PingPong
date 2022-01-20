c0a3d8dd9dfc07c5b64420666a4267a1b4b24b81
almog gueta <almog.gueta1741@gmail.com>
2022-01-20 08:18:51 +0200

    Create Client gets arguments socketType and Protocol type

--- PingPong/Client/ClientFactory.cs
+++ PingPong/Client/ClientFactory.cs
@@ -7,12 +7,12 @@ namespace PingPongClient
 {
     public class ClientFactory
     {
-        public Client CreateClient(string ipEndPoint)
+        public Client CreateClient(string ipEndPoint, SocketType socketType, ProtocolType protocolType)
         {
             IPEndPoint iPEndPoint;
             IPEndPoint.TryParse(ipEndPoint, out iPEndPoint);
 
-            NetSocket netSocket = new NetSocket(SocketType.Stream, ProtocolType.Tcp, iPEndPoint);
+            NetSocket netSocket = new NetSocket(socketType, protocolType, iPEndPoint);
 
             return new Client(iPEndPoint, netSocket);
         }
