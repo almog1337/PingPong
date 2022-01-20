fae23a0b3713e219b1293895f68b1d71ce45ebcd
almog gueta <almog.gueta1741@gmail.com>
2022-01-20 10:23:56 +0200

    Added PingPongClientRunner class

---
+++ PingPong/Client/PingPongClientRunner.cs
@@ -0,0 +1,57 @@
+﻿using Core;
+using Core.Converters;
+using Core.Input.Abstractions;
+using Core.Output.Abstractions;
+using System;
+
+namespace PingPongClient
+{
+    public class PingPongClientRunner
+    {
+        private Client _client;
+        private IInput _userInput;
+        private IOutput _userOutput;
+        private StringToByteArrayConverter _stringToByteArrayConverter;
+        private ByteArrayToStringConverter _byteArrayToStringConverter;
+
+        public PingPongClientRunner(Client client, IInput userInput, IOutput userOutput)
+        {
+            _client = client;
+            _userInput = userInput;
+            _userOutput = userOutput;
+        }
+
+        public void Run()
+        {
+            try
+            {
+                bool run = true;
+                byte[] clientMessage;
+                byte[] serverMessage;
+                _client.Socket.Connect();
+
+                while (_client.Socket.Connected() && run)
+                {
+                    _userOutput.Send(_stringToByteArrayConverter.Convert("Enter Message (Exit to stop): "));
+                    clientMessage = _userInput.Receive();
+
+                    if(_byteArrayToStringConverter.Convert(clientMessage) != "Exit")
+                    {
+                        run = false;
+                    }
+
+                    _client.Socket.Send(clientMessage);
+                    serverMessage = _client.Socket.Receive();
+                }
+            }
+            catch(Exception e)
+            {
+                
+            }
+            finally
+            {
+                _client.Socket.Close();
+            }
+        }
+    }
+}
